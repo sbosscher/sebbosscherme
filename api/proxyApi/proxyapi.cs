5f78107f3f1ad9c663812c70d5a531129aff795c
sbosscher <s.a.bosscher@outlook.com>
2024-01-15 22:23:27 +0000

    converted js to c#

--- api/proxyApi/proxyapi.cs
+++ api/proxyApi/proxyapi.cs
@@ -1,7 +1,24 @@
-const fetch = require('node-fetch');
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
-module.exports = async function (context, req) {
-    const response = await fetch('https://sbosscher-f.azurewebsites.net/api/incrementvaluefunction?code=f5gGUYkK_1kM9baSpu-m5T0FEpxHw2ELZUKjg3EABDg4AzFutBA18Q%3D%3D');
-    const data = await response.text();
-    context.res = { body: data };
-};
+public static class ProxyApi
+{
+    private static readonly HttpClient client = new HttpClient();
+
+    [FunctionName("ProxyApi")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        ILogger log)
+    {
+        log.LogInformation("C# HTTP trigger function processed a request.");
+
+        var response = await client.GetStringAsync("https://sbosscher-f.azurewebsites.net/api/incrementvaluefunction?code=f5gGUYkK_1kM9baSpu-m5T0FEpxHw2ELZUKjg3EABDg4AzFutBA18Q%3D%3D");
+
+        return new OkObjectResult(response);
+    }
+}
